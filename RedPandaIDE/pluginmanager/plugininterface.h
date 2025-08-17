cb92d84f78b9bfef3a6ce1cd6bccef398c1b2daa
GitHub Action <action@github.com>
2025-08-17 13:16:20 +0000

    Update defines

--- RedPandaIDE/pluginmanager/plugininterface.h
+++ RedPandaIDE/pluginmanager/plugininterface.h
@@ -17,9 +17,6 @@ public:
 
     // Called once when the plugin is loaded. MainWindow pointer is provided for
     // plugins that need to interact immediately with the application.
-    // Plugins may call `mainWindow->uiLanguage()` (added in the host) to query
-    // the current UI language code (for example "en_US" or "zh_CN") so they
-    // can load/refresh their own translations.
     virtual void initialize(MainWindow *mainWindow) = 0;
 
     // Return settings pages (caller takes ownership when adding to SettingsDialog)
